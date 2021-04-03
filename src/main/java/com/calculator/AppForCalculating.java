71cf0c5185f9cd646b26247ef7d536c424e98fe3
Leandro Javier Baier <leanbaieruni@gmail.com>
2021-04-03 14:32:16 -0300

    App for billing

--- src/main/java/com/calculator/AppForCalculating.java
+++ src/main/java/com/calculator/AppForCalculating.java
@@ -19,10 +19,14 @@ public class AppForCalculating {
             System.out.println("5. Salir");
             System.out.print("Ingrese una opcion: ");
             opcion = scanner.nextInt();
-            System.out.print("Ingrese primer numero(Solo numero enteros): ");
-            Integer numero1 = scanner.nextInt();
-            System.out.print("Ingrese segundo numero(Solo numero enteros): ");
-            Integer numero2 = scanner.nextInt();
+            Integer numero1 = 0;
+            Integer numero2 = 0;
+            if(opcion < 5){
+                System.out.print("Ingrese primer numero(Solo numero enteros): ");
+                numero1 = scanner.nextInt();
+                System.out.print("Ingrese segundo numero(Solo numero enteros): ");
+                numero2 = scanner.nextInt();
+            }
 
             switch (opcion){
                 case 1:
@@ -37,7 +41,6 @@ public class AppForCalculating {
                 case 4:
                     if(numero2 != 0){
                         System.out.println("El cociente es: " + (Double.valueOf(numero1) / Double.valueOf(numero2)));
-                        System.out.println("Con resto: " + (numero1 % numero2));
                     }else{
                         System.out.println("No se puede dividir por 0!!!");
                     }
